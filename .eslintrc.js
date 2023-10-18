00039c1963f5ff2e86c792670a7c836f21960c94
Tian Feng <tian.feng@saucelabs.com>
2023-10-17 17:12:22 -0700

    Update .eslintrc.js
    Co-authored-by: Alex Plischke <alex.plischke@saucelabs.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,6 @@ module.exports = {
     ],
     rules: {
         semi: "error",
-        "@typescript-eslint/no-explicit-any": "off"
+        "@typescript-eslint/no-explicit-any": "warn"
     }
 };
